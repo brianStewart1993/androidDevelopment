b09724de1c9c0d386b9de6b492d1a43e4b8d9be8
brianStewart1993 <stewart.brian36@yahoo.com>
2017-06-26 16:03:12 -0500

    Add files via upload

---
+++ CreateTask.js
@@ -0,0 +1,133 @@
+function myFunctionTruth() {
+	
+var num = Math.floor((Math.random() * 50) + 1);
+
+ var x = document.getElementById("demo")
+
+if(num == 1) x.innerHTML = 'What is your favourite part of my body?';
+else if (num == 2) x.innerHTML = 'If you could do whatever you wanted to me what would it be?';
+else if (num == 3) x.innerHTML = 'Describe what you would like me to do to you right now!';
+else if (num == 4) x.innerHTML = 'How many orgasms have I given you Before if any?';
+else if (num == 5) x.innerHTML = 'On a scale from 1 to 10 how good is the head that I give you?';
+else if (num == 6) x.innerHTML = 'Do you like giving me head?';
+else if (num == 7) x.innerHTML = 'What is our best sexual experience that you remember?';
+else if (num == 8) x.innerHTML = 'Have we ever had bad sex?';
+else if (num == 9) x.innerHTML = 'Where do you like my tongue the most?';
+else if (num == 10) x.innerHTML = 'Do you like it rough or soft?';
+else if (num == 11) x.innerHTML = 'Do you ever dream about the sex we have together?';
+else if (num == 12) x.innerHTML = 'What is the thing that I do that makes you cum every time?';
+else if (num == 13) x.innerHTML = 'What is the funniest thing that I have ever done during sex?';
+else if (num == 14) x.innerHTML = 'Have you ever faked an orgasim with me?';
+else if (num == 15) x.innerHTML = 'Have I ever done something that you didn’t enjoy during sex?';
+else if (num == 16) x.innerHTML = 'Have you ever not been in the mood, but still had sex because it made me happy?';
+else if (num == 17) x.innerHTML = 'What part of your body would you love for me to massage?';
+else if (num == 18) x.innerHTML = 'Have you ever thought about someone else while we were having sex?';
+else if (num == 19) x.innerHTML = 'Would you ever want to get a stripper pole for our room?';
+else if (num == 20) x.innerHTML = 'What food would you like me to eat off of you?';
+else if (num == 21) x.innerHTML = 'Would you ever cook naked for me?';
+else if (num == 22) x.innerHTML = 'Do you like “doggy-style” or “missionary” better?';
+else if (num == 23) x.innerHTML = 'What sexual position have you always wanted to try?';
+else if (num == 24) x.innerHTML = 'Would you ever want to make a video with me?';
+else if (num == 25) x.innerHTML = 'What’s the most embarrassing thing that has happened while you were having sex?';
+else if (num == 26) x.innerHTML = 'When’s the last time you touched yourself?';
+else if (num == 27) x.innerHTML = 'Have you ever been sexually attracted to someone you mentally knew was ugly?';
+else if (num == 28) x.innerHTML = 'What kind of wild sexual fantasies do you have?';
+else if (num == 29) x.innerHTML = 'Would you be opposed to threesomes?';
+else if (num == 30) x.innerHTML = 'Have you ever had an erection at work/school?';
+else if (num == 31) x.innerHTML = 'Have you ever had a one night stand?';
+else if (num == 32) x.innerHTML = 'If you have had sex in a public place, where?';
+else if (num == 33) x.innerHTML = 'Have you ever cried during/after sex?';
+else if (num == 34) x.innerHTML = 'Have you ever had an orgasm without being touched?';
+else if (num == 35) x.innerHTML = 'Who is the youngest person you have had sex with?';
+else if (num == 36) x.innerHTML = 'Who is the oldest person you have had sex with?';
+else if (num == 37) x.innerHTML = 'Who’s the last person you undressed in your thoughts?';
+else if (num == 38) x.innerHTML = 'What is your favourite sex position?';
+else if (num == 39) x.innerHTML = 'How many people do you think your partner has had sex with?';
+else if (num == 40) x.innerHTML = 'What is something that turns you on, that your partner may not know about?';
+else if (num == 41) x.innerHTML = 'In bed, do you prefer to dominate or to submit?';
+else if (num == 42) x.innerHTML = 'Describe something you wanted to do with your partner in bed, but have been too shy to suggest.';
+else if (num == 43) x.innerHTML = 'Does it turn you on when your partner talks dirty during sex?';
+else if (num == 44) x.innerHTML = 'Ideally, how often would you have sex?';
+else if (num == 45) x.innerHTML = 'Does it turn you on to be spanked during sex?';
+else if (num == 46) x.innerHTML = 'Has anyone ever seen you naked/semi-naked without you meaning them to?';
+else if (num == 47) x.innerHTML = 'If a stranger were to offer money to sleep with your partner, how much money would it need to be for you to agree?';
+else if (num == 48) x.innerHTML = 'What is the most inappropriate time or place that you have had an erection?';
+else if (num == 49) x.innerHTML = 'How many people have you had oral sex with?';
+else if (num == 50) x.innerHTML = 'Have you ever had a threesome? Ever wanted to?';
+};
+
+function myFunctionDare() {
+	
+var num = Math.floor((Math.random() * 50) + 1);
+
+ var x = document.getElementById("demo")
+
+if (num == 1) x.innerHTML = 'Give me head for three full songs, or until I cum, whichever comes first';
+else if (num == 2) x.innerHTML = 'Put on the sexiest song you know, and give me a naked lap dance';
+else if (num == 3) x.innerHTML = 'Give me a hickey on my chest';
+else if (num == 4) x.innerHTML = '(For women) Finger yourself until you cum while I record you';
+else if (num == 5) x.innerHTML = '(For men) Jack off until you cum while I record you';
+else if (num == 6) x.innerHTML = '(For women) Ride my penis for 3 minutes only (if you go longer it’s a penalty)';
+else if (num == 7) x.innerHTML = '(For men) Do me in doggy-style for 3 minutes only (if you go longer it’s a penalty)';
+else if (num == 8) x.innerHTML = 'Make me cum without touching my privates';
+else if (num == 9) x.innerHTML = 'Sit on my lap with no clothes on';
+else if (num == 10) x.innerHTML = 'Grab whatever ever you want out of the fridge, put it on my privates, and suck it off';
+else if (num == 11) x.innerHTML = '(For Women) Try to do a naked split';
+else if (num == 12) x.innerHTML = 'Take off my underwear using only your teeth';
+else if (num == 13) x.innerHTML = 'Let me tie up your hands and feet, then I will give you head, do not make a sound';
+else if (num == 14) x.innerHTML = 'Rub on your nipples until they become hard';
+else if (num == 15) x.innerHTML = 'Play with yourself for the next 3 minutes';
+else if (num == 16) x.innerHTML = 'Moan as loud as you can';
+else if (num == 17) x.innerHTML = 'Let me record you giving me head for the next two minutes';
+else if (num == 18) x.innerHTML = 'Do your best yoga pose naked';
+else if (num == 19) x.innerHTML = 'Pick your favourite porn, let’s watch it together and do exactly what they do';
+else if (num == 20) x.innerHTML = 'Let’s go have sex in the shower';
+else if (num == 21) x.innerHTML = 'Do me in your favourite position';
+else if (num == 22) x.innerHTML = 'Be my sex slave for the next 30 minutes';
+else if (num == 23) x.innerHTML = 'Sing the chorus of your favorite song.';
+else if (num == 24) x.innerHTML = 'Write your name on my privates with your tongue.';
+else if (num == 25) x.innerHTML = 'I dare you to whisper the naughtiest thing you can think of into my ear.';
+else if (num == 26) x.innerHTML = 'I dare you to tease me by going down on me for as long as you can without making me orgasm.';
+else if (num == 27) x.innerHTML = 'I dare you to make-out with me, without feeling me up, for 5 minutes straight.';
+else if (num == 28) x.innerHTML = 'I dare you to fuck me on the nearest surface we can find.';
+else if (num == 29) x.innerHTML = 'I dare you to cover one of your body parts in whipped cream, and then let me lick it off of you.';
+else if (num == 30) x.innerHTML = 'I dare you to go in the bathroom and take the sexiest nudes you can and then send them to me.';
+else if (num == 31) x.innerHTML = 'I dare you to take erotic pictures of me while I suck your dick.';
+else if (num == 32) x.innerHTML = 'I dare you to make me as horny as you possibly can without touching me.'; 
+else if (num == 33) x.innerHTML = 'I dare you to put on a porno I can watch while you eat me out.';
+else if (num == 34) x.innerHTML = 'I dare you to spank me as hard as you possibly can.';
+else if (num == 35) x.innerHTML = 'I dare you to handcuff(tie) me to the bed and have your way with me.';
+else if (num == 36) x.innerHTML = 'I dare you to do as many push-ups as you can. While naked.';
+else if (num == 37) x.innerHTML = 'I dare you to kiss me anywhere, except on my lips.';
+else if (num == 38) x.innerHTML = 'I dare you to fuck me in a position we’ve never tried before.';
+else if (num == 39) x.innerHTML = 'I dare you to send me the dirtiest sext you can come up with.';
+else if (num == 40) x.innerHTML = 'I dare you to guess what color underwear I’m wearing.';
+else if (num == 41) x.innerHTML = 'I dare you to be as loud as you can when you fuck me tonight.';
+else if (num == 42) x.innerHTML = 'I dare you to rub your dick against my clit for as long as you can ';
+else if (num == 43) x.innerHTML = 'I dare you to bring me into the living room and fuck me in the middle of floor.';
+else if (num == 44) x.innerHTML = 'I dare you to watch me undress without getting a hard-on.';
+else if (num == 45) x.innerHTML = 'I dare you to strike the most seductive pose that you can.';
+else if (num == 46) x.innerHTML = 'I dare you to have sex with me for at least a half-hour before you orgasm.';
+else if (num == 47) x.innerHTML = 'I dare you to put on your favorite porno, so I can see what you’re into.';
+else if (num == 48) x.innerHTML = 'I dare you to run your tongue across any area of my body that you choose.';
+else if (num == 49) x.innerHTML = 'I dare you to masturbate at the same time as me.';
+else if (num == 50) x.innerHTML = 'I dare you to undress me with your eyes closed';
+};
+
+function getProjectNames()
+{ var comp =  "test";
+  var dataString = 'company=' + comp; 
+$.ajax({
+url: "getProjectName.php", // Url to which the request is send
+type: "POST",            // Type of request to be send, called as method
+data: dataString, // Data sent to server, a set of key/value pairs (i.e. form fields and values)
+contentType: false,       // The content type used when sending data to the server.
+cache: false,             // To unable request pages to be cached
+processData:false,        // To send DOMDocument or non processed data file it is set to false
+success: function(data)   // A function to be called if request succeeds
+{
+$("#ProjectName").html(data);
+}
+});
+};
+
